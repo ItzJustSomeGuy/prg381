138238292aaa211dd3f4c39f2a5854a983a86dba
ItzJustSomeGuy <sebastiaan.wolfaardt@gmail.com>
2021-02-24 10:28:07 +0200

    something

--- prg381.java
+++ prg381.java
@@ -1,6 +1,7 @@
 public class prg381{
     public static void main(String[] args) {
         System.out.println("First Java file on git");
+        System.out.println("and another one");
         //commentmore
         //another one
     }
